5a82dae80364db26e2282b71cf0f13aaa2334dec
Manikandan Nair <mani4pmn@gmail.com>
2021-03-16 07:40:29 +0000

    Detecting user login/logout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:VirQ/screens/wrapper.dart';
+import 'package:VirQ/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:VirQ/models/user.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,8 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+          child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,15 @@
 import 'package:VirQ/screens/authenticate/authenticate.dart';
 import 'package:VirQ/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:VirQ/models/user.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final user = Provider.of<User>(context);
+    print(user);
  
     return Authenticate(); 
   }
