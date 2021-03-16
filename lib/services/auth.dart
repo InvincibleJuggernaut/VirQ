b69efc86bae2f5bebb68ea0dd867ae884ba5889d
Manikandan Nair <mani4pmn@gmail.com>
2021-03-16 07:23:13 +0000

    Add files via upload

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,19 @@
+import 'package:VirQ/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
   Future signInAnon() async {
      try {
        AuthResult result = await _auth.signInAnonymously();
        FirebaseUser user = result.user;
-       return user;
+       return _userFromFirebaseUser(user);
      } catch(e) {
        print(e.toString());
        return null;
