6d2dddf65a89a066d6ad46ce9fe84dea1e618307
Manikandan Nair <mani4pmn@gmail.com>
2021-03-17 05:49:03 +0000

    Toggle between signin and signup

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -2,6 +2,11 @@ import 'package:VirQ/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
+
+  final Function toggleView;
+  Register({ this.toggleView });
+
+
   @override
   _RegisterState createState() => _RegisterState();
 }
@@ -19,7 +24,16 @@ class _RegisterState extends State<Register> {
       appBar: AppBar(
         backgroundColor: Colors.yellow[900],
         elevation: 0.0,
-        title: Text('SIGNUP'),
+        title: Text('SIGN UP'),
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('SIGN IN'),
+            onPressed: () {
+              widget.toggleView();
+            }
+          )
+        ]
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
@@ -45,7 +59,7 @@ class _RegisterState extends State<Register> {
               RaisedButton(
                 color: Colors.blue[500],
                 child: Text(
-                  'Register',
+                  'Sign Up',
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -2,6 +2,10 @@ import 'package:VirQ/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
+
+  final Function toggleView;
+  SignIn({ this.toggleView });
+
   @override
   _SignInState createState() => _SignInState();
 }
@@ -20,7 +24,16 @@ class _SignInState extends State<SignIn> {
       appBar: AppBar(
         backgroundColor: Colors.yellow[900],
         elevation: 0.0,
-        title: Text('LOGIN'),
+        title: Text('SIGN IN'),
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('SIGN UP'),
+            onPressed: () {
+              widget.toggleView();
+            }
+          )
+        ]
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
