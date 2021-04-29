59684a2c7d3aeba01911fd0bafccba33e0211ff2
Manikandan Nair <mani4pmn@gmail.com>
2021-04-29 05:11:34 +0000

    Update queue_details_form.dart

--- lib/screens/home/queue_details_form.dart
+++ lib/screens/home/queue_details_form.dart
@@ -162,21 +162,3 @@ class _QueueDetailsState extends State<QueueDetails> {
     );
   }
 }
-
-// ignore: must_be_immutable
-class NewScreen extends StatelessWidget {
-  String payload;
-
-  NewScreen({
-    @required this.payload,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(payload),
-      ),
-    );
-  }
-}
